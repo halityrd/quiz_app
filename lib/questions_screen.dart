0a0be4631d4b7a24540e10c3dbde8b86d8413218
halityrd <halityrd@hotmail.com>
2025-08-14 16:11:27 +0300

    Google Fonts

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
 
@@ -31,10 +32,18 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('The question...', style: TextStyle(color: Colors.white)),
+          Text(
+            currentQuestion.text,
+            style: GoogleFonts.lato(
+              color: const Color.fromARGB(255, 201, 153, 251),
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
           const SizedBox(height: 30),
           ...currentQuestion.getShuffledAnswers().map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {});
+            return AnswerButton(answerText: answer, onTap: answerQuestion);
           }),
         ],
       ),
